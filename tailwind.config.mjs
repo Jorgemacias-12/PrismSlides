9a48dced7d8106986a980424df477dfbbe7acf57
Jorgemacias-12 <63885983+Jorgemacias-12@users.noreply.github.com>
2024-12-08 05:23:23 -0600

    update: config with theme colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  darkMode: "selector",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        // Dark theme primary colors
+        "primary-a0": "#056674",
+        "primary-a10": "#327682",
+        "primary-a20": "#4f8691",
+        "primary-a30": "#6897a0",
+        "primary-a40": "#81a7af",
+        "primary-a50": "#9ab9bf",
+
+        // Dark theme surface colors
+        "surface-a0": "#101010",
+        "surface-a10": "#262626",
+        "surface-a20": "#3d3d3d",
+        "surface-a30": "#565656",
+        "surface-a40": "#707070",
+        "surface-a50": "#8a8a8a",
+
+        // Dark theme mixed surface colors
+        "surface-mixed-a0": "#14191a",
+        "surface-mixed-a10": "#292e2f",
+        "surface-mixed-a20": "#404445",
+        "surface-mixed-a30": "#585c5d",
+        "surface-mixed-a40": "#727576",
+        "surface-mixed-a50": "#8c8f90",
+      },
+    },
+  },
+  plugins: [],
+};
